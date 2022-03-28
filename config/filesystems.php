a9edc6cc45a64b90ac4ab275b3d3825d47a445ec
Nikita Ivanov <nikita03.1998@mail.ru>
2022-03-28 08:52:43 +0300

    Внесение нового диска public_html

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,13 @@ return [
             'visibility' => 'public',
         ],
 
+        'public_html' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public_html'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
