c612f6f1473d9b8ff4a85f84a2ed1544a3029015
Nikita Ivanov <therockyholl@gmail.com>
2022-04-03 22:44:02 +0300

    Доработки интеграции yookassa

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use YooKassa\Client;
-use YooKassa\Model\Notification\NotificationSucceeded;
-use YooKassa\Model\Notification\NotificationWaitingForCapture;
+use YooKassa\Model\Notification\NotificationFactory;
 use YooKassa\Model\NotificationEventType;
 
 class PaymentController extends Controller
 {
     public function payCreate(Request $request)
     {
+
+        header('Access-Control-Allow-Origin', '*');
+        header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
         $orderId = $request->order_id;
 
         $clientId = env('YK_ID_TEST');
@@ -26,8 +30,8 @@ class PaymentController extends Controller
                 'value' => env('PRICE'),
                 'currency' => 'RUB',
             ],
-            'description' => 'Оформление ЭПТС. Заказ №'.$orderId,
-            'capture' => true,
+            'description' => 'Оформление ЭПТС. Заказ №' . $orderId,
+            'capture' => false,
             'confirmation' => [
                 'type' => 'redirect',
                 'return_url' => route('pay.callback'),
@@ -37,45 +41,16 @@ class PaymentController extends Controller
             ],
         ], uniqid('', true));
 
-        return redirect( $payment->getConfirmation()->getConfirmationUrl() );
+        return response()->json([
+            'status'=>true,
+            'url'=>$payment->getConfirmation()->getConfirmationUrl()
+        ]);
 
     }
 
 
     public function payCallback(Request $request)
     {
-        $source = file_get_contents('php://input');
-        $requestBody = json_decode($source, true);
-
-
-        $notification = ($requestBody['event'] === NotificationEventType::PAYMENT_SUCCEEDED)
-            ? new NotificationSucceeded($requestBody)
-            : new NotificationWaitingForCapture($requestBody);
-        $payment = $notification->getObject();
-
-
-        if (isset($payment->status) && $payment->status==='succeeded') {
-            if ($payment->paid === true) {
-                info( json_encode($payment->object) );
-
-                echo 'Успех #';
-                echo $payment->metadata->order_id;
-
-                 $order = Order::find($payment->metadata->order_id);
-                    $order->pay_success = 1;
-                    $order->save();
-            }
-        } else {
-            echo 'Провал';
-            # Запись ошибок в лог
-            info( json_encode($request->object) );
-
-            # Пример записи ответа в модель заказа
-            // $order = Order::find($req->object['metadata']['order_id']);
-            //    $order->payment = 'error';
-            //    $order->payment_message = json_encode($req->object);
-            //    $order->save();
-        }
-
+        return redirect('');
     }
 }
