f4941d4ee56bd865ac1c27dba401cf5ec6eb1e51
Nikita Ivanov <therockyholl@gmail.com>
2022-04-05 19:59:42 +0300

    Правка для выдачи чека

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -17,7 +17,23 @@ class PaymentController extends Controller
         header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+        $request->validate([
+            'order_id'=>'required',
+            'type_owner'=>'required',
+            'email'=>'required',
+            'phone'=>'required'
+        ]);
+
         $orderId = $request->order_id;
+        $type_owner = $request->type_owner;
+        $email = $request->email;
+        $phone = $request->phone;
+
+        if ($type_owner == '1') {
+            $name = $request->last_name . ' ' . $request->first_name . ' ' . $request->patronymic;
+        } else {
+            $name = $request->org_name;
+        }
 
         $clientId = env('YK_ID_TEST');
         $clientSecret = env('YK_KEY_TEST');
@@ -30,7 +46,7 @@ class PaymentController extends Controller
                 'value' => env('PRICE'),
                 'currency' => 'RUB',
             ],
-            'description' => 'Оформление ЭПТС. Заказ №' . $orderId,
+            'description' => 'Заказ #' . $orderId . '. Оформление ЭПТС.',
             'capture' => false,
             'confirmation' => [
                 'type' => 'redirect',
@@ -39,11 +55,29 @@ class PaymentController extends Controller
             'metadata' => [
                 'order_id' => $orderId,
             ],
+            'receipt' => [
+                'customer' => [
+                    'full_name' => $name,
+                    'email' => $email,
+                    'phone' => $phone,
+                ],
+                'items' => [
+                    [
+                        'description' => 'Заказ #' . $orderId . '. Оформление ЭПТС.',
+                        'quantity' => '1.00',
+                        'amount' => [
+                            'value' => 5000,
+                            'currency' => 'RUB'
+                        ],
+                        'vat_code' => '3',
+                    ]
+                ]
+            ]
         ], uniqid('', true));
 
         return response()->json([
-            'status'=>true,
-            'url'=>$payment->getConfirmation()->getConfirmationUrl()
+            'status' => true,
+            'url' => $payment->getConfirmation()->getConfirmationUrl()
         ]);
 
     }
@@ -51,6 +85,10 @@ class PaymentController extends Controller
 
     public function payCallback(Request $request)
     {
+        header('Access-Control-Allow-Origin', '*');
+        header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
         return redirect('https://reg.gospts.ru/#/finish');
     }
 }
